e048e04cae54d2405c6aca44b7c0f69042bff031
anousheh21 <anousheh1@gmail.com>
2023-12-26 16:40:11 +0000

    Added basic working functionality of rock paper scissors

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,15 +7,70 @@
 
 import SwiftUI
 
+struct StandardButton: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .buttonStyle(.bordered)
+    }
+}
+
+extension View {
+    func standardButton() -> some View {
+        modifier(StandardButton())
+    }
+}
+
 struct ContentView: View {
+    @State private var options = ["rock", "paper", "scissors"].shuffled()
+    @State private var answer = ""
+    @State private var checkedAnswer = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Button("🪨") {
+                    shuffleOptions()
+                    answer = options[0]
+                    checkAnswer(winAgainst: "scissors", loseAgainst: "paper")
+                }
+                .standardButton()
+                
+                Button("🗞️") {
+                    shuffleOptions()
+                    answer = options[0]
+                    checkAnswer(winAgainst: "rock", loseAgainst: "scissors")
+                }
+                .standardButton()
+                
+                Button("✂️") {
+                    shuffleOptions()
+                    answer = options[0]
+                    checkAnswer(winAgainst: "paper", loseAgainst: "rock")
+                }
+                .standardButton()
+            }
+            
+            Text(answer)
+            Text(checkedAnswer)
+            
         }
         .padding()
+    }
+    
+    // FUNCTIONS
+    func shuffleOptions() {
+        answer = ""
+        options.shuffle()
+    }
+    
+    func checkAnswer(winAgainst win: String, loseAgainst lose: String) {
+        if answer == win {
+            checkedAnswer = "You Win"
+        } else if answer == lose {
+            checkedAnswer = "You Lose"
+        } else {
+            checkedAnswer = "It's a Draw"
+        }
     }
 }
 
